40755aaab3848e5a0458be2e73a18ad80bb89c98
zhoux0130 <zhoux0130@hotmail.com>
2021-06-26 04:02:51 +0000

    finish project

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -7,9 +7,10 @@ import pandas as pd
 # sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
 # from sklearn.externals import joblib
 # Import joblib package directly
-import joblib
+from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
+from sklearn.svm import LinearSVC
 
 
 # Provided model load function
